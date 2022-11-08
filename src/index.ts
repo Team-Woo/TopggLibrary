bf0d715828fea9515a1055b242ea0c4c91ddb695
Zach <zachburke859@gmail.com>
2022-11-08 18:49:09 -0500

    added some small abstraction to the database. Moved hasVoted to parent class. Privated db and interval. Moved addUser to after emit. uncaught errors wont add the user to the database and have topgg repeat the webhook

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,16 @@ export interface TopggOptions {
 export class TopggLib extends EventEmitter {
 	public app: express.Application;
 	public options: TopggOptions;
+
 	private webhook;
-	db: db;
-	public _interval: NodeJS.Timer;
+	private _db: db;
+	private _interval: NodeJS.Timer;
 	constructor(authorization: string, options: TopggOptions) {
 		super();
 		this.app = express();
 		this.options = options;
 
-		this.db = new db(options.dbPath);
+		this._db = new db(options.dbPath);
 
 		this.webhook = new Webhook(authorization);
 
@@ -35,30 +36,38 @@ export class TopggLib extends EventEmitter {
 				return;
 			}
 			*/
-
-			this.db.addUser(vote.user)
-
 			this.emit('vote', vote);
+			this._db.addUser(vote.user)
 		}))
 
 		this.app.listen(this.options.port);
 
 
 		this._interval = setInterval(async () => {
-			this.db.sqlite.each(`
-			SELECT *
-			FROM voters
-			WHERE votedAt <= unixepoch(datetime('now', '-12 hours')) * 1000;
-			`, (err, row) => {
-				if (err) return console.log(err);
-				this.emit('reminder', row)
-
-				this.db.sqlite.run(`
-				DELETE FROM voters
-				WHERE id = ?
-				`, [row.id])
-			})
-		}, this.options.interval || 10000)
+			this._db.reminders(
+				(err, row) => {
+					if (err) return console.error(err);
+					this.emit('reminder', row)
+					this._db.deleteUser(row.id)
+				}
+			)
+		},
+			this.options.interval || 10000);
 
 	}
+
+	/**
+	 * 
+	 * @param id a user or memeber id
+	 * @returns true if the user voted within the past 12 hours
+	 * 
+	 * @example 
+	 * ```js
+	 * 	const voted = await topgg.hasVoted(user.id)
+	 * ```
+	 */
+	async hasVoted(id: string): Promise<boolean> {
+		return ((await this._db.getUser(id)) != undefined)
+	}
+
 }
--- src/structs/database.ts
+++ src/structs/database.ts
@@ -1,26 +1,28 @@
 import sqlite3 from "sqlite3";
 
 export class db {
-	sqlite: sqlite3.Database;
+	public _sqlite: sqlite3.Database;
 	ready: boolean;
 	constructor(path: string) {
 		this.ready = false;
-		this.sqlite = new sqlite3.Database(path, (err) => {
+		this._sqlite = new sqlite3.Database(path, (err) => {
 			if (err) {
 				console.error(err.message);
 			}
 		})
-		this.sqlite.run(`
+		this._sqlite.run(`
 		CREATE TABLE IF NOT EXISTS voters (
 			id VARCHAR NOT NULL PRIMARY KEY,
 			votedAt timestamp
 		);
-		`);
+		`, (err) =>{
+			if(!err) this.ready = true
+		});
 
 	}
 
-	addUser(id: string) {
-		this.sqlite.run(`
+	addUser(id: string): void {
+		this._sqlite.run(`
 		INSERT into voters 
 		(id, votedAt)
 		VALUES (?, ?);
@@ -31,13 +33,32 @@ export class db {
 			])
 	}
 
-	hasVoted(id: string) {
+	deleteUser(id: string): void {
+		this._sqlite.run(`
+		DELETE FROM voters
+		WHERE id = ?
+		`, [id])
+	}
+
+	getUser(id: string): Promise<row> {
 		return new Promise((resolve, reject) => {
-			this.sqlite.get('SELECT * FROM voters WHERE id = ?', id, (err, rows) => {
-				if(err) reject(err);
-				resolve(rows != undefined)
+			this._sqlite.get('SELECT * FROM voters WHERE id = ?', id, (err: Error, row: row) => {
+				if (err) reject(err);
+				resolve(row)
 			})
 		})
-
 	}
+
+	reminders(callback: (err: Error, row: row) => void) {
+		this._sqlite.each(`
+		SELECT *
+		FROM voters
+		WHERE votedAt <= unixepoch(datetime('now', '-1 minute')) * 1000;
+		`, callback)
+	}
+}
+
+interface row {
+	id: string
+	votedAt: number
 }
